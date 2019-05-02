8a210b89d112e5fbc1081fb245d549301a66d53c
jjkazm <jjkazm@gmail.com>
2019-05-02 23:56:02 +0200

    Implement full_name method

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  def full_name
+    last_name.upcase + ", " + first_name.upcase
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = create(:user)
+  end
   describe "creation" do
-    before do
-      @user = create(:user)
-    end
     it "can be created" do
       expect(@user).to be_valid
     end
@@ -13,6 +13,12 @@ RSpec.describe User, type: :model do
       @user.last_name = nil
       @user.first_name = nil
       expect(@user).not_to be_valid
+    end
+  end
+
+  describe "custom name methods" do
+    it "has full name that combines first and last name" do
+      expect(@user.full_name).to eq("OGOREK, JUREK")
     end
   end
 end
