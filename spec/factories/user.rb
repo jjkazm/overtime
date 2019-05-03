8f11020aeaa353001b66e4ca40ee5aaee7876a83
jjkazm <jjkazm@gmail.com>
2019-05-03 16:22:23 +0200

    Add sequence email in user factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,15 +1,27 @@
 FactoryBot.define do
+
+  sequence :email do |n|
+    "person#{n}@example.com"
+  end
+
   factory :user do
-    email { "asdf@user.com" }
+    email
     password { "asdfjkl;" }
     first_name { "Jurek" }
     last_name { "Ogorek" }
   end
 
-  factory :admin_user, class: "AdminUser" do
-    email { "second@user.com" }
+  factory :user, class: "User" do
+    email
     password { "asdfjkl;" }
     first_name { "Second" }
+    last_name { "User" }
+  end
+
+  factory :admin_user, class: "AdminUser" do
+    email
+    password { "asdfjkl;" }
+    first_name { "Admin" }
     last_name { "User" }
   end
 end
